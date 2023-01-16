6bf4efff42de4f8d91fe7ac5a3c9b38cfac6a8f4
Estrella <110181623+esordone@users.noreply.github.com>
2023-01-16 10:55:17 +0100

    Add files via upload

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: esordone <esordone@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 15:37:13 by esordone          #+#    #+#             */
-/*   Updated: 2023/01/12 15:49:08 by esordone         ###   ########.fr       */
+/*   Updated: 2023/01/16 10:53:15 by esordone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,16 @@ int main (int argc, char **agrv)
 	int i;
 
 	i = 0;
-	if (argc >= 2)
+	if (argc < 2)
 	{
-		while 
+		return (0);
+		write (1, "\n", 1);
 	}
-	write (1, "\n", 1);
+	//com argv[0] es a.out, al posar ++i ja comenca en argv[1],
+	//i va passant fins que aplegue a l'ultim argument
+	while (argv[++i])
+	{
+
+	}
 	return (0);
 }
